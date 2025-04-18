5a2bdf9a207d88d8bec9a54e184937e74a42b5e0
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-04-18 02:42:37 -0400

    star

--- instrumentation/helidon/testing/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-  id("otel.java-conventions")
-}
-
-dependencies {
-  api(project(":testing-common"))
-  implementation enforcedPlatform("io.helidon:helidon-dependencies:4.2.0")
-  implementation 'io.helidon.webserver:helidon-webserver'
-}
